e6d4c533011de0b19d26f12280e8874ff5b8c813
Mark Devenyi <markdevenyidev@gmail.com>
2025-09-30 02:53:23 -0400

    feat(modmgr): add filesystem watcher for module hot-reload

---
+++ internal/modmgr/fswatcher.go
@@ -0,0 +1,88 @@
+package modmgr
+
+import (
+	"context"
+	"io/fs"
+	logger "omnirouter/internal"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func LookForChanges(rootpath string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		logger.Error("FSNotify setup failed", "error", err)
+		return
+	}
+	defer watcher.Close()
+
+	root := filepath.Clean(rootpath)
+
+	_ = watcher.Add(root)
+	_ = filepath.WalkDir(root, func(path string, d fs.DirEntry, walkErr error) error {
+		if walkErr != nil {
+			/* Ignore invalid subdirs (permission issues) */
+			return fs.SkipDir
+		}
+		if d != nil && d.IsDir() {
+			_ = watcher.Add(path)
+		}
+		return nil
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				p := filepath.Clean(event.Name)
+
+				if event.Has(fsnotify.Create) || event.Has(fsnotify.Rename) || event.Has(fsnotify.Chmod) {
+					if fi, err := os.Stat(p); err == nil && fi.IsDir() {
+						if err := watcher.Add(p); err == nil {
+							logger.Debug("Watch added", "dir", p)
+						}
+					}
+				}
+
+				if event.Has(fsnotify.Write) {
+					st, err := os.Stat(event.Name)
+					if err != nil {
+						logger.Warn("stat() returned error while checking file entry WRITE event")
+						continue
+					}
+
+					if !st.IsDir() {
+						logger.Debug("File modified", "path", p)
+					}
+				}
+
+				if event.Has(fsnotify.Remove) || event.Has(fsnotify.Rename) {
+					err = watcher.Remove(p)
+					if err == nil {
+						logger.Debug("Watch removed", "path", p)
+					}
+				}
+
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				logger.Error("Error from FSNotify watcher", "error", err)
+
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	<-make(chan struct{})
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	logger "omnirouter/internal"
 	"omnirouter/internal/config"
+	"omnirouter/internal/modmgr"
 
 	"github.com/rs/zerolog"
 )
@@ -12,4 +13,5 @@ func main() {
 	logger.SetLevel(zerolog.DebugLevel)
 	logger.Info("OmniRouter started!")
 	config.ParseConfig("examples/config.toml")
+	modmgr.LookForChanges("examples/modules/")
 }
