00615dece11d54f3e77897d286e1d83e0eccdf66
Mark Devenyi <markdevenyidev@gmail.com>
2025-10-21 02:09:53 -0400

    feat(modmgr): add `or_register_http()` to register handlers for endpoints from C

--- internal/modmgr/modloader.go
+++ internal/modmgr/modloader.go
@@ -12,8 +12,25 @@ import "C"
 
 import (
 	"omnirouter/internal/logger"
+	"omnirouter/internal/router"
 	"unsafe"
 )
+
+type cHandler struct {
+	fn    C.or_http_handler_t
+	extra unsafe.Pointer
+}
+
+var _ router.HTTPHandler = cHandler{}
+
+func (h cHandler) Invoke(ctx router.ContextPtr, req router.RequestPtr) {
+	C.call_or_http_handler(
+		h.fn,
+		(*C.or_ctx_t)(ctx),
+		(*C.or_http_req_t)(req),
+		h.extra,
+	)
+}
 
 func loadModule(path string) {
 	logger.Info("Checking modloaders...")
@@ -26,3 +43,12 @@ func loadModule(path string) {
 	defer C.free(unsafe.Pointer(cpath))
 	C.cffi_load_module(cpath)
 }
+
+//export or_register_http
+func or_register_http(path *C.char, handler C.or_http_handler_t, extra unsafe.Pointer) C.uint64_t {
+	goPath := C.GoString(path)
+	logger.Info("Added new HTTP handler for path ", "path", goPath)
+	router.GetHTTPRouter().Register(goPath, cHandler{fn: handler, extra: extra})
+	return C.uint64_t(1)
+}
+
