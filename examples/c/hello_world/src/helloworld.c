bf8f950989653900160f60932a75804e69b22173
Mark Devenyi <markdevenyidev@gmail.com>
2025-10-21 02:54:25 -0400

    style(examples): remove newline from C hello_world log messages

--- examples/c/hello_world/src/helloworld.c
+++ examples/c/hello_world/src/helloworld.c
@@ -5,13 +5,12 @@
 static void (*loginfo_)(char* msg, char* module_);
 
 void hello_world_handler(or_ctx_t* ctx, or_http_req_t* req, void* extra) {
-    loginfo_("Hello World triggered!\n", LOCATION);
+    loginfo_("Hello World triggered!", LOCATION);
 }
 
 bool init(const or_api_t* api) {
     api->register_http("/", hello_world_handler, NULL);
-    api->loginfo("Hello from the dynamically loaded library!\n", LOCATION);
-
+    api->loginfo("Hello from the dynamically loaded library!", LOCATION);
     loginfo_ = api->loginfo;
     return true;
 }
