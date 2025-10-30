937cdb720d70a87ab03a41001d8e64f9b184fa1f
Mark Devenyi <markdevenyidev@gmail.com>
2025-10-30 18:53:33 -0400

    feat(modmgr): add `or_unregister_http()` C FFI function

--- internal/modmgr/modloader.go
+++ internal/modmgr/modloader.go
@@ -52,3 +52,9 @@ func or_register_http(path *C.char, handler C.or_http_handler_t, extra unsafe.Po
 	return C.uint64_t(1)
 }
 
+//export or_unregister_http
+func or_unregister_http(path *C.char) {
+	goPath := C.GoString(path)
+	logger.Info("Unregistered HTTP handler for path", "path", goPath)
+	router.GetHTTPRouter().Unregister(goPath)
+}
