eae8a456e7dc7f92eba9beb0e1f500846c5b6e32
Mark Devenyi <markdevenyidev@gmail.com>
2025-11-07 06:33:28 -0500

    fix(caps): first actual capability should start from 1

--- internal/capabilities/capabilities.go
+++ internal/capabilities/capabilities.go
@@ -3,11 +3,12 @@ package capabilities
 type Capabilities uint64
 
 const (
-	CAP_LOGGING                Capabilities = 1 << 0
-	CAP_LOGGING_FATAL          Capabilities = 1 << 1
-	CAP_HTTP_REGISTER          Capabilities = 1 << 2
-	CAP_HTTP_REGISTER_WILDCARD Capabilities = 1 << 3
-	CAP_HTTP_UNREGISTER        Capabilities = 1 << 4
+	cAP_NONE                   Capabilities = 0
+	CAP_LOGGING                Capabilities = 1 << 1
+	CAP_LOGGING_FATAL          Capabilities = 1 << 2
+	CAP_HTTP_REGISTER          Capabilities = 1 << 3
+	CAP_HTTP_REGISTER_WILDCARD Capabilities = 1 << 4
+	CAP_HTTP_UNREGISTER        Capabilities = 1 << 5
 )
 
 func HasCapabilities(capset Capabilities, capabilities Capabilities) bool {
