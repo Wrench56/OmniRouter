0599648613628574024dcf5358fd0736b402cb2b
Mark Devenyi <markdevenyidev@gmail.com>
2025-11-07 06:35:55 -0500

    feat(router): add better error reporting to `Register()` and `Unregister()`

--- internal/router/router.go
+++ internal/router/router.go
@@ -27,10 +27,19 @@ type HTTPHandler interface {
 }
 
 type HTTPRouter interface {
-	Register(caps capabilities.Capabilities, path string, h HTTPHandler)
-	Unregister(caps capabilities.Capabilities, path string)
+	Register(caps capabilities.Capabilities, path string, h HTTPHandler) uint64
+	Unregister(caps capabilities.Capabilities, path string) uint64
 	Lookup(path string) (HTTPHandler, bool)
 }
+
+const (
+	SUCCESS = 0
+	/* Do not use error code 1! */
+	ERR_FFI_RESERVED = 1
+	ERR_REG_CAP      = 2
+	ERR_REG_WILD_CAP = 3
+	ERR_UNREG_CAP    = 2
+)
 
 func setup() {
 	routerOnce.Do(func() {
@@ -64,16 +73,19 @@ func NewHTTPRouter() HTTPRouter {
 	return &radixRouter{tree: radix.New()}
 }
 
-func (r *radixRouter) Register(caps capabilities.Capabilities, path string, h HTTPHandler) {
+func (r *radixRouter) Register(caps capabilities.Capabilities, path string, h HTTPHandler) uint64 {
 	if !capabilities.HasCapabilities(caps, capabilities.CAP_HTTP_REGISTER) {
-		return
+		logger.Warn("Insufficient capabilities to register an HTTP route", "capabilities", caps, "needed", capabilities.CAP_HTTP_REGISTER)
+		return ERR_REG_CAP
 	}
 
 	p := normalize(path)
 
 	if strings.HasSuffix(p, "*") {
 		if !capabilities.HasCapabilities(caps, capabilities.CAP_HTTP_REGISTER_WILDCARD) {
-			return
+
+			logger.Warn("Insufficient capabilities to register a wildcard HTTP route", "capabilities", caps, "needed", capabilities.CAP_HTTP_REGISTER_WILDCARD&capabilities.CAP_HTTP_REGISTER)
+			return ERR_REG_WILD_CAP
 		}
 		p = strings.TrimSuffix(p, "*")
 		if !strings.HasSuffix(p, "/") {
@@ -84,17 +96,22 @@ func (r *radixRouter) Register(caps capabilities.Capabilities, path string, h HT
 	r.mu.Lock()
 	r.tree.Insert(p, h)
 	r.mu.Unlock()
+	logger.Info("Added new HTTP handler for path ", "path", path)
+	return SUCCESS
 }
 
-func (r *radixRouter) Unregister(caps capabilities.Capabilities, path string) {
+func (r *radixRouter) Unregister(caps capabilities.Capabilities, path string) uint64 {
 	if !capabilities.HasCapabilities(caps, capabilities.CAP_HTTP_UNREGISTER) {
-		return
+		logger.Warn("Insufficient capabilities to unregister an HTTP route", "capabilities", caps, "needed", capabilities.CAP_HTTP_REGISTER)
+		return ERR_UNREG_CAP
 	}
 	p := normalize(path)
 
 	r.mu.Lock()
 	r.tree.Delete(p)
 	r.mu.Unlock()
+	logger.Info("Unregistered HTTP handler for path", "path", path)
+	return SUCCESS
 }
 
 func (r *radixRouter) Lookup(path string) (HTTPHandler, bool) {
